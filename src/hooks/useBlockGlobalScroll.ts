3d74d63a61d7b0004b121db8f598e1847d716177
Miroga64 <miroga64gmail.com>
2024-11-22 12:10:15 +0300

    Fix scroll

--- src/hooks/useBlockGlobalScroll.ts
+++ src/hooks/useBlockGlobalScroll.ts
@@ -2,14 +2,12 @@ import { useEffect, useMemo } from "react"
 
 const useBlockGlobalScroll = () => {
     const setGlobalScroll = useMemo(() => (scrollBehavior: string) => {
-        console.log('scrollBehavior: 0',scrollBehavior)
-        document.body.style.overflowY = scrollBehavior
+        document.documentElement.style.overflowY = scrollBehavior
     }, [])
 
     useEffect(() => {
         return () => {
-            console.log('scrollBehavior: 1')
-            document.body.style.overflowY = 'scroll'
+            document.documentElement.style.overflowY = 'scroll'
         }
     }, [])
 
